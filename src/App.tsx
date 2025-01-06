fbd825d1601b6cd90e13fa3b34ed5ef2c1d6d147
NotUnlikeTheWaves <hleistra@gmail.com>
2025-01-06 18:28:27 +0100

    Deal with FE stuff (only gigabrains will understand and I'm not one of them)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { WordHolder } from './WordHolder'
 import { GameState } from './Types'
+import { WordLength } from './Constants'
+
+
 
 
 function App() {
-  const [gameState, setGameState] = useState<GameState>({currentWord: "aha", history: ["zesty", "lemon"]})
+const [gameState, setGameState] = useState<GameState>({currentWord: "", history: ["zesty", "lemon"]})
+  
+function handleUserInput(event: KeyboardEvent) {
+  console.log("handle input")
+  console.log(event)
+  function isLetter(s: string) : boolean {
+    return s.length == 1 && (s.toLowerCase() != s.toUpperCase())
+  }
+  let key = event.key.toUpperCase()
+  switch(key) {
+    case "ENTER": {
+      // handle submit
+      break;
+    }
+    case "BACKSPACE": {
+      let newCurrentWord = gameState.currentWord.slice(0, -1)
+      setGameState({...gameState, currentWord: newCurrentWord})
+      break;
+    }
+    default: {
+      if(isLetter(key) && gameState.currentWord.length < WordLength) {
+        let newCurrentWord = gameState.currentWord + key;
+        setGameState({...gameState, currentWord: newCurrentWord})
+      }
+      break;
+    }
+  }
+
+}
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleUserInput)
+    return () => document.removeEventListener('keydown', handleUserInput)
+  }, [gameState.currentWord]) // magic
 
   return (
     <>
